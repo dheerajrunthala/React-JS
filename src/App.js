b25f651265cc433ba37e08a0a7b7e285a4843eec
dheerajrunthalaff <dheeraj.runthala@fiftyfivetech.io>
2023-12-19 16:36:23 +0530

    Things tested on related to react

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       {/* learn react with me  */}
+    //       Learn React with DHEERAJ
+    //     </a>
+    //   </header>
+    // </div>
+  // );
+  
+  
+  
+  return (
+    <div className="App">
+    Helllo  ji 
+  </div>
+  
+
+  )
+}
+
+export default App;
